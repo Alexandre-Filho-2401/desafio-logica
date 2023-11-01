82fc4a038b4f5a2dbd352fcdc31a0e0b13835752
Alexandre-Filho-2401 <alexandrefilho2401@gmail.com>
2023-11-01 19:22:19 -0300

    testando adição aleatória de xp

--- index.js
+++ index.js
@@ -1,1 +1,14 @@
 //Desafio de código 1 - Proressão do Heroi
+//Usar uma estrutura de decisão para dar uma classificação ao nível de XP do Heroi
+
+let Heroi = "Valdemiro";
+let Classes = ["Bronze", "Prata", "Ouro", "Platina", "Ascendente", "Imortal", "Radiante"];
+let xp = 0;
+
+let x = Math.floor(Math.random()*21);
+
+for(let i=1;i<=x;i++){
+    xp += 500;
+}
+
+console.log(xp);
